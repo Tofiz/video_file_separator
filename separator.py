e83f7db97ee86c6691e640185cb23e8a39dacbf1
Tofiz <tofi.zagon@gmail.com>
2018-02-12 13:38:23 +0200

    for demo

--- separator.py
+++ separator.py
@@ -1,7 +1,8 @@
-import os
+import os, sys
 import shutil
 
-path = "/Users/Tofi/Desktop/TESZT/Díjlovaglás Kitti négszög 2/"
+path = sys.argv[1] if len(sys.argv) == 2 else r'.'
+# path = "C:/Users/Tofi/Desktop/TESZT/Díjlovaglás Kitti négszög 2/"
 os.chdir(path) 
 names = os.listdir(path)
 elements = len(names)
@@ -21,13 +22,14 @@ def selector():
             pass
         elif os.stat(files).st_size < limit:
             shutil.move(path+files, path+'other/'+files)
-        
-        
+
+   
 
 limit = limit()
 selector()
 moved_files = len(os.listdir(path+"other/"))
 print(limit, "bytes is the limit. \n", moved_files," element(s) moved to the 'other' folder.\nNow you can run the rename.py! ;) ")
+
 
 
 
